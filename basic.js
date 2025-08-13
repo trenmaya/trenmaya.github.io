3ae9d691ea2d78936a54769a95e140f80c25b1d9
leartoler <fabiangg@protonmail.ch>
2025-08-13 12:05:32 -0500

    Prueba no validada

--- basic.js
+++ basic.js
@@ -4,37 +4,30 @@ window.onload = () => {
     const el = document.querySelector("[gps-new-camera]");
 
     el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntityAdded) {
+        if (!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const boxEntity = document.createElement("a-box");
-            boxEntity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            boxEntity.setAttribute('material', { color: 'red' } );
-            boxEntity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            });
 
-            const circleEntity = document.createElement("a-sphere");
-            circleEntity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            circleEntity.setAttribute('material', { color: 'green' } );
-            circleEntity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude - 0.001,
-                longitude: e.detail.position.longitude
+            const scene = document.querySelector("a-scene");
+
+            // Array of objects to add
+            const objects = [
+                { latOffset: 0.001, lonOffset: 0, color: 'red' },
+                { latOffset: 0, lonOffset: 0.001, color: 'blue' },
+                { latOffset: -0.001, lonOffset: -0.001, color: 'green' }
+            ];
+
+            objects.forEach(obj => {
+                const entity = document.createElement("a-box");
+                entity.setAttribute("scale", { x: 20, y: 20, z: 20 });
+                entity.setAttribute('material', { color: obj.color });
+                entity.setAttribute('gps-new-entity-place', {
+                    latitude: e.detail.position.latitude + obj.latOffset,
+                    longitude: e.detail.position.longitude + obj.lonOffset
+                });
+                scene.appendChild(entity);
             });
 
-
-
-            document.querySelector("a-scene").appendChild(entity);
+            testEntityAdded = true;
         }
-        testEntityAdded = true;
     });
 };
