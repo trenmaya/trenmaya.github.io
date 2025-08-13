9a838e24520fc5c03688ff3a8430f019ab85e029
leartoler <fabiangg@protonmail.ch>
2025-08-13 11:51:11 -0500

    Prueba GPS 202508131010 Poner 4 en cada uno de los puntos cardinales

--- basic.js
+++ basic.js
@@ -7,84 +7,34 @@ window.onload = () => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
-            entity.setAttribute("scale", {
+            const boxEntity = document.createElement("a-box");
+            boxEntity.setAttribute("scale", {
                 x: 20, 
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('material', { color: 'red' } );
-            entity.setAttribute('gps-new-entity-place', {
+            boxEntity.setAttribute('material', { color: 'red' } );
+            boxEntity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
-            document.querySelector("a-scene").appendChild(entity);
-        }
-        testEntityAdded = true;
-    });
 
-el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntityAdded) {
-            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-sphere");
-            entity.setAttribute("scale", {
+            const circleEntity = document.createElement("a-sphere");
+            circleEntity.setAttribute("scale", {
                 x: 20, 
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('material', { color: 'yellow' } );
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude,
-                longitude: e.detail.position.longitude + 0.001
-            });
-            document.querySelector("a-scene").appendChild(entity);
-        }
-        testEntityAdded = true;
-    });
-
-el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntityAdded) {
-            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-cylinder");
-            entity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'orange' } );
-            entity.setAttribute('gps-new-entity-place', {
+            circleEntity.setAttribute('material', { color: 'green' } );
+            circleEntity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude - 0.001,
                 longitude: e.detail.position.longitude
             });
+
+
+
             document.querySelector("a-scene").appendChild(entity);
         }
         testEntityAdded = true;
     });
-
-el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntityAdded) {
-            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-cone");
-            entity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'magenta' } );
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude - 0.001,
-                longitude: e.detail.position.longitude
-            });
-            document.querySelector("a-scene").appendChild(entity);
-        }
-        testEntityAdded = true;
-    });
-
-
-
-
-
 };
